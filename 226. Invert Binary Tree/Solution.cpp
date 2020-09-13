4022740bf16fe4a1edf5eb29e7a2d4272fbae9c6
xzhao <30533752+robinzx117@users.noreply.github.com>
2020-09-12 23:41:48 -0700

    Add files via upload

---
+++ 226. Invert Binary Tree/Solution.cpp
@@ -0,0 +1,47 @@
+/*
+Invert a binary tree.
+
+Example:
+Input:
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+Output:
+
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(root == NULL){
+            return NULL;
+        }
+        TreeNode *temp;
+        temp = root->right;
+        root->right = root->left;
+        root->left = temp;
+        invertTree(root->left);
+        invertTree(root->right);
+        return root;
+    }
+};
