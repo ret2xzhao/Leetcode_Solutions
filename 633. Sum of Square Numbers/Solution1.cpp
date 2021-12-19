df9eddeb63a71dd647ea83a6370e2203642da138
xzhao <30533752+robinzx117@users.noreply.github.com>
2021-12-18 16:42:56 -0800

    Create Solution1.cpp

---
+++ 633. Sum of Square Numbers/Solution1.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+        long left = 0;
+        long right = (long)sqrt(c);
+        while(left<=right){
+            long sum = left*left + right*right;
+            if(sum == c){
+                return true;
+            }
+            else if(sum>c){
+                --right;
+            }
+            else{
+                ++left;
+            }
+        }
+        return false;
+    }
+};
