0d231d56a2ca793d03a3457966106cc2ef24b3d7
xzhao <30533752+robinzx117@users.noreply.github.com>
2022-01-12 00:59:32 -0800

    Update Q1.cpp

--- Interview/C++/Q1.cpp
+++ Interview/C++/Q1.cpp
@@ -1,13 +1,12 @@
-#include <iostream>
-#include <string>
+#include <stdio.h>
 
 int main(){
-    char * s = "12345";
-    printf("%d\n",      s);
-    printf("%d\n",     *s);
-    printf("%c\n",     *s);
-    printf("%c\n", *(s+1));
-    printf("%s\n",      s);
-    printf("%s\n",     *s);
+    char* s = "12345";
+    printf("%d\n",      s); // Convert the address of the string into decimal.
+    printf("%d\n",     *s); // 49 - '1' in decimal.
+    printf("%c\n",     *s); // 1
+    printf("%c\n", *(s+1)); // 2
+    printf("%s\n",      s); // 12345
+    printf("%s\n",     *s); // warning: format ‘%s’ expects argument of type ‘char *’, but argument 2 has type ‘int’ [-Wformat=]
     return 0;
 }
